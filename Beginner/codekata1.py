a2186a6a9871d36ea2a3648a4e4e32986a5e771f
shalinipjohncena <36122963+shalinipjohncena@users.noreply.github.com>
2018-02-04 16:27:53 +0530

    Add files via upload

---
+++ Beginner/codekata1.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module5
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a=input()
+if a>0 and a<100000:
+     print("positive number")
+elif a<0:
+        print("negative number")
+elif a==0:
+       print("zero")
+else:
+           print("invalid num")
